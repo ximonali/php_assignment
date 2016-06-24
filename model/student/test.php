8deb2250ff6bc3e67e8119cb32fedc67e7ff1018
leoMouraNet <laercio.zdq@gmail.com>
2016-06-24 01:19:47 -0400

    Updating Model Test

--- model/student/test.php
+++ model/student/test.php
@@ -12,23 +12,32 @@ class ModelStudentTest extends Model {
 		return $test->row;
 	}
 
-	function getQuestion($test_id) {
+	//Return N randon questions ($total_question) from the test_id 
+	function getQuestions($test_id, $total_question) {
+		$student_id = $this->student->getID();
 
 	}
 
+	//return 4 choices
 	function getChoices($question_id) {
 
 	}
 
+	//insert the answer chose
 	function setAnswer($question) {
+		$student_id = $this->student->getID();
 
 	}
 
+	//insert on table student_test which test and student starting
 	function startTest($test_id) {
+		$student_id = $this->student->getID();
 
 	}
 
+	//update on table student_test the test over and return if user pass (true or false)
 	function endTest($test_id) {
+		$student_id = $this->student->getID();
 
 	}
 
