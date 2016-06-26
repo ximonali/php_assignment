e57fe9fcf7cf2ed3e66c551c58c605c2383dfb21
Simon Gonzalez <ximonali@gmail.com>
2016-06-26 14:21:30 -0400

    done

--- admin/view/user/test_center.php
+++ admin/view/user/test_center.php
@@ -11,6 +11,63 @@
 			<?php endforeach; ?>
 		</ul>
 
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Course Name</th>
+        <th>Description</th>
+        <th>Duration</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="success">
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+        <td> 
+          <button type="button" class="btn btn-default btn-sm">
+          <span class="glyphicon glyphicon glyphicon-trash" aria-hidden="true"></span> Del
+          </button>
+
+          <button type="button" class="btn btn-default btn-sm">
+          <span class="glyphicon glyphicon glyphicon glyphicon-pencil" aria-hidden="true"></span> Edit
+          </button>
+        </td>
+      </tr>
+      <tr class="danger">
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+        <td>
+          <button type="button" class="btn btn-default btn-sm">
+          <span class="glyphicon glyphicon glyphicon-trash" aria-hidden="true"></span> Del
+          </button>
+
+          <button type="button" class="btn btn-default btn-sm">
+          <span class="glyphicon glyphicon glyphicon-pencil" aria-hidden="true"></span> Edit
+          </button>
+
+        </td>
+      </tr>
+      <tr class="info">
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+        <td>
+          <button type="button" class="btn btn-default btn-sm">
+          <span class="glyphicon glyphicon glyphicon-trash" aria-hidden="true"></span> Del
+          </button>
+
+          <button type="button" class="btn btn-default btn-sm">
+          <span class="glyphicon glyphicon glyphicon glyphicon-pencil" aria-hidden="true"></span> Edit
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+
 
       </div>
 
